33b9f3a3b82f536ef01a50b6534136534a4caa55
Luizlf07 <luizfelipetavares187@gmail.com>
2026-08-24 16:20:41 -0300

    test: corrige linter e quebra regra de negocio no teste

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,20 +1,19 @@
-const x = 100; // Variavel sem uso para forcar erro no ESLint
 function somar(a, b) {
-    return a + b;
+  return a - b; // Erro de logica proposital
 }
 
 function subtrair(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiplicar(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function dividir(a, b) {
-    if (b === 0) {
+  if (b === 0) {
     throw new Error("Divisao por zero nao permitida");
-    }
+  }
   return a / b;
 }
 
