427340ee0c32f81d087ecb9b98e2621f061c8786
Luizlf07 <luizfelipetavares187@gmail.com>
2026-08-24 16:14:40 -0300

    primeiro commit do pipeline

---
+++ src/calculadora.js
@@ -0,0 +1,20 @@
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b === 0) {
+    throw new Error("Divisao por zero nao permitida");
+    }
+  return a / b;
+}
+
+module.exports = { somar, subtrair, multiplicar, dividir };
